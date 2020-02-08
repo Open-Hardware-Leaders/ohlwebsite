4ea4e6fea4dbc451f7d1a141204b3eedb1e6c873
jurra@github.com <jose.urra86@gmail.com>
2020-02-08 10:12:12 +0100

    added "help us to improve this page"  for contrib

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,26 @@ module.exports = {
         nav: [
             { text: 'Program', link: '/Program/01_introduction.md' },
         ],
-        sidebar: sidebarArray
+        sidebar: sidebarArray,
+
+        // Assumes GitHub. Can also be a full GitLab url.
+        repo: 'https://github.com/Open-Hardware-Leaders/ohlwebsite',
+        // Customising the header label
+        // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+        repoLabel: 'Repository',
+
+        // Optional options for generating "Edit this page" link
+
+        // if your docs are in a different repo from your main project:
+        docsRepo: 'https://github.com/Open-Hardware-Leaders/ohlwebsite',
+        // if your docs are not at the root of the repo:
+        docsDir: 'docs',
+        // if your docs are in a specific branch (defaults to 'master'):
+        docsBranch: 'master',
+        // defaults to false, set to true to enable
+        editLinks: true,
+        // custom text for edit link. Defaults to "Edit this page"
+        editLinkText: 'Help us improve this page!'
     }
 };
 
